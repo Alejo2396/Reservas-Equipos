e387d6cf0b32f0c295ab18d65a735fc4f91f867e
Alejo2396 <alejomore2396@gmail.com>
2025-12-16 11:34:05 -0500

    Actualizacioness

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
-const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static('frontend'));
-app.use('/api', routes);
 
-app.listen(3000,()=>console.log('Servidor corriendo en http://localhost:3000'));
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.use(require('./routes'));
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
